6b5b890ae4340afc5ec92b8753ef9bcbd44780f5
Kartik Chhabra <129652398+kartikchhabra7@users.noreply.github.com>
2023-12-20 14:15:59 +0530

    updated commit

--- PrivateMemberWithClosure.js
+++ PrivateMemberWithClosure.js
@@ -28,3 +28,32 @@ counter.decrementValue();
 console.log(counter.getCountValue());
 
 console.log("privateMemberValue is ", counter.privateMember); //it will shows "undefined (private variable is not directly accessible)"
+
+
+
+/*------------------------------------------------------------------------------
+or we can use object destructing to avoid extra variables and hard lines of code.
+-------------------------------------------------------------------------------*/
+
+
+const privateMemberFun = () => {
+  let privateMember = 0;
+  const incrementValue = () => privateMember++;
+  const decrementValue = () => privateMember--;
+  const getCountValue = () => privateMember;
+
+  return { incrementValue, decrementValue, getCountValue };
+};
+const { incrementValue, decrementValue, getCountValue, privateMember } =
+  privateMemberFun();
+
+incrementValue();
+incrementValue();
+incrementValue();
+incrementValue();
+
+decrementValue();
+console.log(`Count Value is ${getCountValue()}`);
+console.log(
+  `privateMemberValue is ${privateMember} it works like a private variable`
+);
